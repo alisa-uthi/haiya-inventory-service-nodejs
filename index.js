8bf617cac54d775783e96a54b367dd44a3066b6f
kimberlinn99 <alisa.uth99@gmail.com>
2021-12-09 18:20:34 +0700

    add cache

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
-const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
-const passport = require('passport')
-require('./config/passport')
-const connection = require('./config/database')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const passport = require("passport");
+require("./config/passport");
+const connection = require("./config/database");
+const app = express();
 
-if(process.env.NODE_ENV != 'production'){
-    require('dotenv').config()
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+  app.use(morgan("dev"));
 }
 
 // Connect to database
-connection
+connection;
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-app.use(cors())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
 
 // Routes
-app.use('/product', passport.authenticate('jwt', {session: false}), require('./routes/product_route'))
-app.use('/category', passport.authenticate('jwt', {session: false}), require('./routes/category_route'))
-app.use('/pharmacy', passport.authenticate('jwt', {session: false}), require('./routes/pharmacy_route'))
+app.use(
+  "/product",
+  passport.authenticate("jwt", { session: false }),
+  require("./routes/product_route")
+);
+app.use(
+  "/category",
+  passport.authenticate("jwt", { session: false }),
+  require("./routes/category_route")
+);
+app.use(
+  "/pharmacy",
+  passport.authenticate("jwt", { session: false }),
+  require("./routes/pharmacy_route")
+);
 
-const PORT = process.env.PORT || 8001
+const PORT = process.env.PORT || 8001;
 
 app.listen(PORT, () => {
-    console.log(`Supplier Inventory Service listening on ${PORT}`)
-})
+  console.log(`Supplier Inventory Service listening on ${PORT}`);
+});
--- routes/pharmacy_route.js
+++ routes/pharmacy_route.js
@@ -1,97 +1,126 @@
-const express = require('express')
-const router = express.Router()
-const upload = require('../config/multer')
-const bucket = require('../config/firebase')
+const express = require("express");
+const router = express.Router();
+const upload = require("../config/multer");
+const bucket = require("../config/firebase");
+const nodeCache = require("node-cache");
+const cache = new nodeCache({ stdTTL: 60 * 60 * 1 });
 
-const pharmacyService = require('../services/pharmacy_service')
-const optimeService = require('../services/optime_service')
+const pharmacyService = require("../services/pharmacy_service");
+const optimeService = require("../services/optime_service");
 
 // Get all pharmacies
-router.post('/', async (req, res) => {
-    const { latitude, longitude } = req.body
-    const authorizationToken = req.headers.authorization
-    try {
-        const result = await pharmacyService.getAllPharmacies(latitude, longitude, authorizationToken)
-        res.status(200).json({ data: result })
-    } catch (error) {
-        res.status(500).json({ error: error.message })
+router.post("/", async (req, res) => {
+  const { latitude, longitude } = req.body;
+  const authorizationToken = req.headers.authorization;
+  try {
+    //   Get data from cache
+    const cacheResult = cache.get("allPharmacies");
+    if (cacheResult != undefined) {
+      return res.status(200).json({ data: cacheResult, cache: true });
     }
-})
+
+    const result = await pharmacyService.getAllPharmacies(
+      latitude,
+      longitude,
+      authorizationToken
+    );
+    cache.set("allPharmacies", result);
+
+    return res.status(200).json({ data: result });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 // Get pharmacy by name
-router.get('/', async (req, res) => {
-    try {
-        const result = await pharmacyService.getPharmacyByName(req.query.name)
-        res.status(200).json({ data: result })
-    } catch (error) {
-        res.status(500).json({ error: error.message })
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const result = await pharmacyService.getPharmacyByName(req.query.name);
+    res.status(200).json({ data: result });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 // Get pharmacy by id
-router.get('/:pharId', async (req, res) => {
-    const authorizationToken = req.headers.authorization
-    try {
-        const result = await pharmacyService.getPharmacyById(req.params.pharId, authorizationToken)
-        res.status(200).json({ data: result })
-    } catch (error) {
-        res.status(500).json({ error: error.message })
-    }
-})
+router.get("/:pharId", async (req, res) => {
+  const authorizationToken = req.headers.authorization;
+  try {
+    const result = await pharmacyService.getPharmacyById(
+      req.params.pharId,
+      authorizationToken
+    );
+    res.status(200).json({ data: result });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 // Get nearest pharmacies
-router.post('/nearest', async (req, res) => {
-    const { latitude, longitude } = req.body
-    const authorizationToken = req.headers.authorization
-    try {
-        const result = await pharmacyService.getNearestPharmacies(latitude, longitude, authorizationToken)
-        res.status(200).json({ data: result })
-    } catch (error) {
-        res.status(500).json({ error: error.message })
+router.post("/nearest", async (req, res) => {
+  const { latitude, longitude } = req.body;
+  const authorizationToken = req.headers.authorization;
+  try {
+    //   Get data from cache
+    const cacheResult = cache.get("nearestPharmacies");
+    if (cacheResult != undefined) {
+      return res.status(200).json({ data: cacheResult, cache: true });
     }
-})
+
+    const result = await pharmacyService.getNearestPharmacies(
+      latitude,
+      longitude,
+      authorizationToken
+    );
+    cache.set("nearestPharmacies", result);
+
+    return res.status(200).json({ data: result });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 // Get operation time of the pharmacy
-router.get('/:pharId/optime', async (req, res) => {
-    try {
-        const result = await optimeService.getOptByPharmacyId(req.params.pharId)
-        res.status(200).json({ data: result })
-    } catch (error) {
-        res.status(500).json({ error: error.message })
-    }
-})
+router.get("/:pharId/optime", async (req, res) => {
+  try {
+    const result = await optimeService.getOptByPharmacyId(req.params.pharId);
+    res.status(200).json({ data: result });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 // Update pharmacy image by pharmacy id
-router.patch('/:pharId/image', upload.single('image'), async (req, res) => {
-    const folder = 'pharmacy'
-    const fileName = `${Date.now()}${req.file.originalname}`
-    const filePath = `${folder}/${fileName}`
-    const fileUpload = bucket.file(filePath);
-    const blobStream = fileUpload.createWriteStream({
-      metadata: {
-        contentType: req.file.mimetype
-      }
-    });
-   
-    blobStream.on('error', (error) => {
-      res.status(405).json({ error: error.message });
-    });
-  
-    blobStream.on('finish', async () => {
-      try {
-        const options = {
-          action: 'read',
-          expires: '12-31-2030'
-        };
-        var url = await fileUpload.getSignedUrl(options)
-        await pharmacyService.updatePharmacyImage(req.params.pharId, url[0])
-        res.status(200).json({ message: "The image has been updated." })
-      } catch(error) {
-        res.status(500).json({ error: error.message })
-      }
-    });
-  
-    blobStream.end(req.file.buffer);
-})
+router.patch("/:pharId/image", upload.single("image"), async (req, res) => {
+  const folder = "pharmacy";
+  const fileName = `${Date.now()}${req.file.originalname}`;
+  const filePath = `${folder}/${fileName}`;
+  const fileUpload = bucket.file(filePath);
+  const blobStream = fileUpload.createWriteStream({
+    metadata: {
+      contentType: req.file.mimetype,
+    },
+  });
 
-module.exports = router
+  blobStream.on("error", (error) => {
+    res.status(405).json({ error: error.message });
+  });
+
+  blobStream.on("finish", async () => {
+    try {
+      const options = {
+        action: "read",
+        expires: "12-31-2030",
+      };
+      var url = await fileUpload.getSignedUrl(options);
+      await pharmacyService.updatePharmacyImage(req.params.pharId, url[0]);
+      res.status(200).json({ message: "The image has been updated." });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+  blobStream.end(req.file.buffer);
+});
+
+module.exports = router;
